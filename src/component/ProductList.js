40ec34b62f8969901c6a50bbdcba58c77f31eed3
Pankaj Sarkar <pankajsarkarweb456@gmail.com>
2025-02-18 17:59:13 +0530

    add some styling 1

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Sort from "./Sort";
 import Product from "./Product";
-import FilterSection from "./FilterSection";
+import FilterSection from "./FIlterSection";
 
 const ProductList = () => {
   return (
     <div className="bg-gray-100 pt-[10vh] min-h-screen">
       <div className="container mx-auto px-5 lg:px-10 py-5">
-        
         <div className="flex flex-col lg:flex-row gap-8">
-          
           {/* Filter Section (Sticky Sidebar) */}
           <aside className="w-full lg:w-1/4 rounded-lg sticky top-[12vh] h-fit">
             <FilterSection />
@@ -17,18 +15,16 @@ const ProductList = () => {
 
           {/* Product List Section */}
           <section className="w-full lg:w-3/4 flex flex-col h-[80vh]">
-            
             {/* Sorting Section (Sticky Top) */}
             <div className="rounded-lg mb-4 sticky top-[12vh] z-20">
               <Sort />
             </div>
-            
+
             {/* Product Grid/List (Scrollable) */}
             <div className=" rounded-lg flex-1 overflow-y-auto">
               <Product />
             </div>
           </section>
-
         </div>
       </div>
     </div>
@@ -36,4 +32,3 @@ const ProductList = () => {
 };
 
 export default ProductList;
-
